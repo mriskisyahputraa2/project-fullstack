ad0e1f99d57e7600f271e33e6887b796520f4ac2
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-06-24 11:10:41 +0700

    update

--- backend/controllers/Product.js
+++ backend/controllers/Product.js
@@ -1,7 +1,47 @@
 import Products from "../models/ProductModel.js";
+import User from "../models/UsersModel.js";
 
-export const getProducts = () => {};
-export const getProductById = () => {};
-export const createProduct = () => {};
-export const updateProduct = () => {};
-export const deleteProduct = () => {};
+export const getProducts = async (req, res) => {
+  try {
+    let response;
+    if (req.role === "admin") {
+      response = await Products.findAll({
+        include: [
+          {
+            model: User,
+          },
+        ],
+      });
+    } else {
+      response = await Products.findAll({
+        where: {
+          userId: req.userId,
+        },
+        include: [
+          {
+            model: User,
+          },
+        ],
+      });
+    }
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+export const getProductById = async (req, res) => {};
+export const createProduct = async (req, res) => {
+  const { name, price } = req.body;
+  try {
+    await Products.create({
+      name: name,
+      price: price,
+      userId: req.userId,
+    });
+    res.status(200).json({ msg: "Product created successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+export const updateProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {};
