c34ca5348fa8982036154f17cde031380303d202
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-06-26 13:21:57 +0700

    add auth login for frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,12 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import "bulma/css/bulma.css";
+import axios from "axios";
 
-const root = createRoot(document.getElementById("root"));
+axios.defaults.withCredentials = true; // axios untuk menerima API req & res dari pengguna
+
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
